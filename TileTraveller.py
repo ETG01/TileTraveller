ce7f32cab7723fdf2083dd8bf7e6d732d530d93d
Einar Tómas G Einar Tómas G <einar2250@gmail.com>
2021-09-16 10:50:02 +0000

    byrjun

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,1 +1,18 @@
-print("hello amos")
+list_of_Directions = "NSWE"
+position = 1
+victory = False
+
+def directions(way, locationation):
+    if way == "N":
+        locationation = locationation + 1
+    elif way == "S":
+        locationation = locationation - 1
+    elif way == "W":
+        locationation = locationation - 3
+    elif way == "E":
+        locationation = locationation + 3
+    return locationation
+
+
+def error(locationation):
+
